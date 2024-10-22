089bccd81e926862c660f3355637f1ad2bdf53d3
Joao Vitor Ferreira <42494721+MestreSol@users.noreply.github.com>
2024-10-22 11:08:28 -0300

    Refactor project structure and remove unnecessary files and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 const gulp = require('gulp');
 const imagemin = () => import('gulp-imagemin');
-const sass = require('gulp-sass')(require('sass'));
+const less = require('gulp-less');
 
 gulp.task('images', async function() {
     const imageminModule = await imagemin();
-    return gulp.src('source/img/*')
+    return gulp.src('source/images/*')
         .pipe(imageminModule.default())
         .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('styles', function() {
-    return gulp.src('source/sass/*.scss')
-        .pipe(sass())
+    return gulp.src('source/less/*.less')
+        .pipe(less())
         .pipe(gulp.dest('dist/css'));
 });
 
